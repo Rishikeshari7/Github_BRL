4801f2ce5ba5a9c3a0805dfeaacb821370339481
Rishikeshari7 <rishikeshari7@gmail.com>
2023-11-15 12:56:44 +0530

    added project

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import Loader from './Loader';
 import './App.css';
 function App() {
-  const [username, setUsername] = useState('');
-  const [userData, setUserData] = useState(null);
-  const [followersData, setFollowersData] = useState([]);
-  const [bio, setBio] = useState('');
-  const [location, setLocation] = useState('');
-  const [social, setSocial] = useState('');
-  const [createdAt, setCreatedAt] = useState('');
-  const [publicRepos, setPublicRepos] = useState(0);
-  const [publicGists, setPublicGists] = useState(0);
-  const [following, setFollowing] = useState(0);
-  const [followers, setFollowers] = useState(0);
-  const [loading , setLoading]=useState(false);
+  const [username,setUsername] =useState('');
+  const [userData,setUserData] =useState(null);
+  const [followersData,setFollowersData] =useState([]);
+  const [bio,setBio] =useState('');
+  const [location,setLocation] =useState('');
+  const [social,setSocial] =useState('');
+  const [createdAt,setCreatedAt] =useState('');
+  const [publicRepos,setPublicRepos] =useState(0);
+  const [publicGists,setPublicGists] =useState(0);
+  const [following,setFollowing] =useState(0);
+  const [followers,setFollowers] =useState(0);
+  const [loading,setLoading]=useState(false);
+  const [organizations,setOrganizations] =useState([]);
+  
 
-  async function fetchData(url) {
-    try {
+  async function fetchData(url){
+    try{
       const response = await fetch(url);
       return await response.json();
     } catch (error) {
@@ -24,13 +26,14 @@ function App() {
     }
   }
 
-  async function fetchGitHubUserData() {
+  async function fetchGitHubUserData(){
     const apiURL = `https://api.github.com/users/${username}`;
     setLoading(true);
-    try {
+    try{
       const userData = await fetchData(apiURL);
       showUserDetails(userData);
       await fetchFollowers(username);
+      await fetchOrganisation(username);
     } catch (error) {
       console.log("error");
     }
@@ -39,13 +42,19 @@ function App() {
     }
   }
 
-  async function fetchFollowers(username) {
+  async function fetchFollowers(username){
     const followersDataURL = `https://api.github.com/users/${username}/followers`;
     const data = await fetchData(followersDataURL);
     setFollowersData(data);
   }
+  async function fetchOrganisation(username){
+    const orgURL= `https://api.github.com/users/${username}/orgs`;
+    const orgData= await fetchData(orgURL);
+    setOrganizations(orgData);
 
-  function showUserDetails(userData) {
+  }
+
+  function showUserDetails(userData){
     setUserData(userData);
     setBio(userData.bio || 'No bio available');
     setLocation(userData.location || 'Not specified');
@@ -80,12 +89,24 @@ function App() {
           <p>Followers: {followers}</p>
         </div>
       )}
-
+        
+          {organizations.length===0 ? (<h2>"Create Organisation first  😂"</h2>) : (
+            <div className='orgsContainer'>
+              <h2>Organisation</h2>
+              <div> 
+              {organizations.map((orgs)=>(
+                <p key={orgs.id}>{orgs.login}</p>
+              ))}
+              </div>
+            </div>
+          )}
+        
       <div id="followersList">
         {followersData.map(follower => (
           <div key={follower.id} className="follower-item">
             <img src={follower.avatar_url}/>
-            <p>{follower.login}</p>
+            <p><a href={follower.html_url}  target="_blank">{follower.login}</a></p>
+            
           </div>
         ))}
       </div>
